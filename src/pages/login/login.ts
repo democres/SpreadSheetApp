3b5d2252fb2d2ea7931a0e6e3a512384e2c27a0e
root <root@localhost.localdomain>
2019-04-08 07:53:13 -0400

    Add register button

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -18,12 +18,23 @@ export class LoginPage {
 
   login(): void {
 
-    if (this.username == "localAdmin" && this.password == "1uch2019$"){
+    if (this.username == "localAdmin" && this.password == "123456"){
         this.navCtrl.push(TabsPage);
     } else {
       alert("Wrong User/Password");
     }
 
   }
+
+  register(): void {
+
+    // if (this.username == "localAdmin" && this.password == "123456"){
+        this.navCtrl.push(TabsPage);
+    // } else {
+    //   alert("Wrong User/Password");
+    // }
+
+  }
+
 
 }
